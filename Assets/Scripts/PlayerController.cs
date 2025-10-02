24d3922becccd4025903f3f011684f268992ace3
jasimkun <lse061123@gmaill.com>
2025-10-02 11:55:38 +0900

    [Feat] 퀘스트 시스템

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -40,6 +40,8 @@ public class PlayerController : MonoBehaviour
     private bool wasGrounded;         //직전 프레임에 땅에 있었는지 판단
     private float attackTimer;
 
+    private bool isUImode = false;    //UI 모드 설정
+
     // Start is called before the first frame update
     void Start()
     {
@@ -50,12 +52,19 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        CheckGrounded();
-        HandleLanding();
-        HandleMovement();
-        UpdateAnimator();
-        HandleAttack();
-        HandleJump();
+        if (Input.GetKeyUp(KeyCode.Tab))
+        {
+            ToggleCursorlock();
+        }
+        if (!isUImode)
+        {
+            CheckGrounded();
+            HandleLanding();
+            HandleMovement();
+            UpdateAnimator();
+            HandleAttack();
+            HandleJump();
+        }
     }
 
     void HandleMovement()               //이동 함수 제작
@@ -196,5 +205,32 @@ public class PlayerController : MonoBehaviour
                 animator.SetTrigger("attackTrigger");
             }
         }
+    }
+
+    public void SetCursorLock(bool lockCursor)          //마우스 락 설정 함수
+    {
+        if(lockCursor)
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+            isUImode = false;
+        }
+        else
+        {
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+            isUImode = true;
+        }
+    }
+
+    public void ToggleCursorlock()
+    {
+        bool ShouldLock = Cursor.lockState != CursorLockMode.Locked;
+        SetCursorLock(ShouldLock);
+    }
+
+    public void SetUIMode(bool uiMode)
+    {
+        SetCursorLock(!uiMode);
     }
 }
